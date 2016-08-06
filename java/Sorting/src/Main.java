6f10a704e753993072588126bb3199f9e8691577
vganesan97 <vishaalganesan@Vishaals-MacBook-Pro.local>
2016-08-06 02:34:35 -0700

    finished insertion

---
+++ java/Sorting/src/Main.java
@@ -0,0 +1,41 @@
+/**
+ * Created by vishaalganesan on 8/6/16.
+ */
+
+import java.util.Random;
+import java.lang.Object;
+
+public class Main {
+
+    public static void toString(int[] arr)
+    {
+        for (int j=0; j<arr.length-1; j++)
+        {
+            System.out.print(arr[j]+ " ");
+
+        }
+    }
+    public static void main(String[] args)
+    {
+
+        Random r= new Random();
+        Sorting_Algos sorting_algos=new Sorting_Algos();
+
+        int[] arr=new int[10];
+        int l=arr.length;
+
+        for (int i=0; i<l-1; i++)
+        {
+            int x = r.nextInt(100);
+            arr[i] = x;
+        }
+
+        System.out.print("Unsorted Array: ");
+        System.out.println();
+        toString(arr);
+        System.out.print("Sorted Array: ");
+        //sorting_algos.bubble(arr);
+        sorting_algos.insertion(arr);
+        toString(arr);
+    }
+}
--- java/Sorting/src/Sorting_Algos.java
+++ java/Sorting/src/Sorting_Algos.java
@@ -6,9 +6,9 @@ public class Sorting_Algos
 {
 
     //simple swap method to be used in following methods
-    public void swap(int i, int j, int[] arr)
+    public static void swap(int i, int j, int[] arr)
     {
-        int temp;
+        int temp=0;
         temp=arr[i];
         arr[i]=arr[j];
         arr[j]=temp;
@@ -17,21 +17,41 @@ public class Sorting_Algos
     //sorting from min to max
 
     //bubble sort
-    public int[] bubble(int[] arr)
+    public static int[] bubble(int[] arr)
     {
         int l=arr.length;
 
-        for (int i = 0; i <=l; i++)
+        for (int i = 0; i <l-1; i++)
         {
-            for (int j=0; j<=l-i; j++)
+            for (int j=0; j<l-1-i; j++)
             {
                 if (arr[j] > arr[j + 1])
                 {
-                    swap(arr[j], arr[j+ 1], arr);
+                    swap(j, j+ 1, arr);
 
                 }
             }
         }
+        return arr;
+    }
+
+    //insertion sort
+    public static int[] insertion(int[] arr)
+    {
+        int l=arr.length;
+
+        for (int i=1; i<l; i++)
+        {
+            int k=arr[i];
+            int j=i-1;
+
+            while (j>=0 && k<arr[j])
+            {
+                swap(j,j+1,arr);
+                j--;
+            }
+        }
+
         return arr;
     }
 
